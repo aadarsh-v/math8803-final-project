387cb2458b531460ba498bc8c83e4f101a44584f
ohammondlee <owenhammondlee@gmail.com>
2026-04-15 22:45:56 -0400

    added looping for samples

--- main.py
+++ main.py
@@ -11,32 +11,45 @@ def build_model(config):
         raise ValueError(f"Unknown model: {config['model']}")
 
 def main():
+    compiled_results={}
     config = load_config("configs.yaml")
-    set_seed(config.get("seed", 0))
-    task = build_task(config)
-
-    config["input_dim"] = task.input_dim
-    config["output_dim"] = task.output_dim
-    config["seq_len"] = task.seq_len
-
-    if task.dt is not None:
-        config["dt"] = task.dt
-
-    inputs0 = task.get_reference_batch(config)
-    inputs0 = inputs0.to(config.get("device", "cpu"))
-    exp_name = config["experiment"]
-
-    if exp_name == "rank_sweep":
-        results = sweep_rank(config, task, build_model, inputs0)
-    elif exp_name == "spectral_sweep":
-        results = sweep_spectral(config, task, build_model, inputs0)
-    else:
-        raise ValueError(f"Unknown experiment: {exp_name}")
     
-    mode = config["task_mode"]
-    task_name = config["task"]
+    for i in range(config.get("sample_size",1)):
+
+        
+        set_seed(config.get("seed", 0)+i)
+        task = build_task(config)
+
+        config["input_dim"] = task.input_dim
+        config["output_dim"] = task.output_dim
+        config["seq_len"] = task.seq_len
+
+        if task.dt is not None:
+            config["dt"] = task.dt
+
+        inputs0 = task.get_reference_batch(config)
+        inputs0 = inputs0.to(config.get("device", "cpu"))
+        exp_name = config["experiment"]
+
+        if exp_name == "rank_sweep":
+            results = sweep_rank(config, task, build_model, inputs0)
+        elif exp_name == "spectral_sweep":
+            results = sweep_spectral(config, task, build_model, inputs0)
+        else:
+            raise ValueError(f"Unknown experiment: {exp_name}")
+        
+        # ---- aggregation step ----
+        for param, metrics in results.items():
+            if param not in compiled_results:
+                compiled_results[param] = {m: [] for m in metrics}
+
+            for metric_name, value in metrics.items():
+                compiled_results[param][metric_name].append(value)
+
+        mode = config["task_mode"]
+        task_name = config["task"]
     save_results(
-        results,
+        compiled_results,
         save_dir="results",
         filename=f"{exp_name}_{mode}_{task_name}.json"
     )
